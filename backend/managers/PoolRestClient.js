8d24726859a34a08df79861675a5f868ada9ed5f
guym <guym@gigaspaces.com>
2014-03-13 14:13:29 +0200

    using default headers.

--- backend/managers/PoolRestClient.js
+++ backend/managers/PoolRestClient.js
@@ -5,56 +5,60 @@ var conf = require('../Conf');
 var Client = require('node-rest-client').Client;
 var client = new Client;
 
-function _getBaseUrl(){
+function _getBaseUrl() {
     return conf.poolRestUrl.protocol + "://" + conf.poolRestUrl.domain + ":" + conf.poolRestUrl.port;
 }
 
-function _url( relativePath ){
-    var result =  _getBaseUrl() + relativePath;
+function _url(relativePath) {
+    var result = _getBaseUrl() + relativePath;
     logger.debug('calling [%s]', result);
     return result;
 }
 
 
-function Call(){
+function Call() {
 
-    function _callbackWrapper( callback ){
-        return function( data, response){
-            if ( response.statusCode == 200 ){
+    function _callbackWrapper(callback) {
+        return function (data, response) {
+            if (response.statusCode == 200) {
                 callback(null, data);
-            }else{
+            } else {
                 logger.info('got an error from rest client', data, response);
                 callback(new Error([data, response]))
             }
         }
     }
 
-    this.post = function( url, args, callback ){
-        logger.info('POST: ' , arguments);
+    this.post = function (url, args, callback) {
+        logger.info('POST: ', arguments);
         client.post(url, args, _callbackWrapper(callback));
     };
 
-    this.get = function( url, args, callback ){
-        logger.info('GET: ' , arguments);
-        client.get(url,args, _callbackWrapper(callback));
+    this.get = function (url, args, callback) {
+        logger.info('GET: ', arguments);
+        client.get(url, args, _callbackWrapper(callback));
     };
 
 }
 
 var call = new Call();
 
-
-
-
-exports.createAccounts = function( poolKey, callback ){
-    logger.info('creating account');
-    call.post(_url('/admin/accounts'), {"headers": {"AccountUuid" : poolKey}}, callback);
+var _defaultHeaders = function (poolKey) {
+    return {
+        "headers": { "AccountUuid": poolKey }
+    }
 };
 
-exports.readAccounts = function( poolKey, callback ){
+
+exports.createAccounts = function (poolKey, callback) {
+    logger.info('creating account');
+    call.post(_url('/admin/accounts'), _defaultHeaders(poolKey), callback);
+};
+
+exports.readAccounts = function (poolKey, callback) {
     logger.info('get accounts called on pool rest client');
 
     // TODO base url to conf or something
-    call.get( _url('/admin/accounts'), {"headers": {"AccountUuid" : poolKey}}, callback);
+    call.get(_url('/admin/accounts'), _defaultHeaders(poolKey), callback);
 
 };

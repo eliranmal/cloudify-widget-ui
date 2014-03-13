cf06752c1884ac650fb249c29059252eb45ecb1f
guym <guym@gigaspaces.com>
2014-03-13 16:27:37 +0200

    adding some pool admin functions to controller

--- backend/controllers/PoolController.js
+++ backend/controllers/PoolController.js
@@ -2,17 +2,34 @@ var logger = require('log4js').getLogger('PoolController');
 
 var managers = require('../managers');
 
-exports.readUsers = function (req, res) {
-    logger.info('users called in pool controller');
-    managers.poolClient.readAccounts( req.user.poolKey, function( err, accounts ){
-        res.send(accounts);
-    });
-};
-
+function _callback( res, callback ){
+    return function( err, data ){
+        if ( !!err ){
+            res.send(err.response.statusCode, data);
+        }
+        if ( !!callback ){
+            callback(data);
+        }
+        else{
+            res.send(data);
+        }
+    }
+}
 
 exports.createUsers = function( req, res ){
-    logger.info('creating new user');
-    managers.poolClient.createAccounts( req,user,poolKey, function ( err, account){
-        res.send(account);
-    });
+    managers.poolClient.createAccounts( req.user.poolKey, _callback ( res ) )
 };
+
+exports.readUsers = function (req, res) {
+    managers.poolClient.readAccounts( req.user.poolKey, _callback( res ) )
+};
+
+exports.readPools = function ( req, res ){
+    managers.poolClient.readPools (req.user.poolKey, _callback(res ) );
+};
+
+exports.readAccountPools = function( req, res ){
+    managers.poolClient.readAccountPools( req.user.poolKey, req.params.accountId, _callback(res))
+};
+
+

506970c1fd5b607d5f431e971df1144bd37e5be3
guym <guym@gigaspaces.com>
2014-03-26 22:07:08 +0200

    changing version.

--- build/install.sh
+++ build/install.sh
@@ -1,7 +1,14 @@
 upgrade_main(){
 
-
+    wget --no-cache --no-check-certificate -O - http://get.gsdev.info/gsat/1.0.0/install_gsat.sh | dos2unix | bash
 
     source /opt/gsat/gsui_functions.sh
+
+    install_mysql
+
+    install_node
+
+    mkdir /opt/cloudify-widget-ui
+    run_wget -O /opt/cloudify-widget-ui/cloudify-widget-ui-1.0.0.tgz http://get.gsdev.info/cloudify-widget-ui/1.0.0/cloudify-widget-ui-1.0.0.tgz
 }
 upgrade_main

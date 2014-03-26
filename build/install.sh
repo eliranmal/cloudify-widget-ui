f4d4466660c83ace791bf5965d369a573d78ad1b
guym <guym@gigaspaces.com>
2014-03-27 00:33:22 +0200

    changing version.

--- build/install.sh
+++ build/install.sh
@@ -17,13 +17,17 @@ install_main(){
 }
 
 upgrade_main(){
+     eval "`wget --no-cache --no-check-certificate -O - http://get.gsdev.info/gsat/1.0.0/install_gsat.sh | dos2unix`"
 
-
-     mkdir /opt/cloudify-widget-ui
+     mkdir -p /opt/cloudify-widget-ui
      npm install http://get.gsdev.info/cloudify-widget-ui/1.0.0/cloudify-widget-ui-1.0.0.tgz -g --prefix /opt/cloudify-widget-ui
 
      dos2unix /opt/cloudify-widget-ui/**/*.sh
      chmod +x /opt/cloudify-widget-ui/**/*.sh
+
+    INSTALL_LOCATION=/opt/cloudify-widget-ui/lib/node_modules/cloudify-widget-ui
+     echo "installing service script under widget-pool"
+     SERVICE_NAME=widget-ui SERVICE_FILE=$INSTALL_LOCATION/build/service.sh install_initd_script
 
 }
 

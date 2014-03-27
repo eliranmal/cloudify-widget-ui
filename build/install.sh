648e95a4daa38ec47bbc30021b9f7d629b0b0ed7
guym <guym@gigaspaces.com>
2014-03-27 15:22:29 +0200

    changing version.

--- build/install.sh
+++ build/install.sh
@@ -1,5 +1,5 @@
 # use this script by running
-# wget --no-cache --no-check-certificate -O - http://get.gsdev.info/cloudify-widget-ui/1.0.0/install.sh | dos2unix | bash
+# yum -y install dos2unix && wget --no-cache --no-check-certificate -O - http://get.gsdev.info/cloudify-widget-ui/1.0.0/install.sh | dos2unix | bash
 
 
 install_main(){

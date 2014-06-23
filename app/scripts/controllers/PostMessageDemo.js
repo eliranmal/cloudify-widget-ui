dfa6eceb69405aa8ecb2ba131ae757c675d3b50e
eliranmal <eliranmal@gmail.com>
2014-06-23 11:59:09 +0300

    CW-77 decoupling state from the widget controller

--- app/scripts/controllers/PostMessageDemo.js
+++ app/scripts/controllers/PostMessageDemo.js
@@ -3,14 +3,27 @@
 angular.module('cloudifyWidgetUiApp')
     .controller('PostMessageDemoCtrl', function ($scope, WidgetsService, $routeParams, $window, $location, $log, $timeout) {
 
+        // using timeout only to test if deferred assignment of the url works with the iframe
         $timeout(function () {
             $scope.blankIframeSrc = 'http://localhost.com:9000/#/postmessagedemo/widgets/1/blank';
         }, 1000);
 
         $scope.postPlay = function () {
+            _postMessage({name: 'play', widget: {_id: 'abc'}, advancedParams: {}, isRemoteBootstrap: false});
+        };
+
+        $scope.postStop = function () {
+            _postMessage({name: 'stop', widget: {_id: 'abc'}, executionId: 'abc', isRemoteBootstrap: false});
+        };
+
+        $scope.getOutput = function () {
+            _postMessage({name: 'getOutput', widget: {_id: 'abc'}, executionId: 'abc'});
+        };
+
+        function _postMessage (data) {
             $log.info('posting message to widget api frame');
 //            $log.info('origin: ', $window.location.origin);
-            $window.frames[0].postMessage('play', $window.location.origin);
+            $window.frames[0].postMessage(data, $window.location.origin);
         };
 
         $window.addEventListener('message', function (e) {
--- app/scripts/controllers/Widget.js
+++ app/scripts/controllers/Widget.js
@@ -20,178 +20,71 @@ angular.module('cloudifyWidgetUiApp')
 
         // TODO extract all state related behavior to widget-theme-controllers (e.g. DefaultCtrl)
 
-        $window.$windowScope = $scope;
+        function getStatus (widget, executionId) {
 
-        $scope.collapseAdvanced = false;
-        $scope.widgetStatus = {};
-        var play = 'RUNNING';
-        var stop = 'STOPPED';
-        var ellipsisIndex = 0;
-
-
-        $scope.showPlay = function () {
-            return $scope.widgetStatus.state === stop;
-        };
-
-        $scope.showStop = function () {
-            return $scope.widgetStatus.state === play;
-        };
-
-
-        function _resetWidgetStatus() {
-            $scope.widgetStatus = {
-                'state': stop,
-                'reset': true
-            };
+            WidgetsService.getStatus( widget, executionId ).then(function (result) {
+                if (!result) {
+                    return false;
+                }
+                $log.debug('posting status');
+                _postMessage('status', result);
+            }, function (result) {
+                $log.error(['status error', result]);
+            });
         }
 
-        _resetWidgetStatus();
+        function play (widget, advancedParams, isRemoteBootstrap) {
 
-        function _hasAdvanced() {
-            return $scope.advancedParams;
-        }
-
-        function _getAdvanced() {
-            return $scope.advancedParams;
-        }
-
-        function _scrollLog(){
-            var log = $('#log')[0];
-            log.scrollTop = log.scrollHeight;
-        }
-
-        function _handleStatus( status, myTimeout ) {
-
-            $log.info(['got status', status]);
-            $localStorage.widgetStatus = status;
-            ellipsisIndex = ellipsisIndex +1;
-            $scope.widgetStatus = status;
-            _getOutput($scope.widget);
-
-            $timeout(_pollStatus, myTimeout || 3000);
-
-            _scrollLog();
-        }
-
-        function _pollStatus(myTimeout) {
-
-            if ($scope.widgetStatus.state !== stop) { // keep polling until widget stops ==> mainly for timeleft..
-                WidgetsService.getStatus( $scope.widget, $scope.executionId ).then(function (result) {
-                    if (!result) {
-                        return;
-                    }
-                    _handleStatus(result.data, myTimeout);
-                }, function (result) {
-                    $log.error(['status error', result]);
-                });
-            } else {
-                $log.info('removing widget status');
-                delete $localStorage.widgetStatus;
-            }
-        }
-
-        // use this with the following from the popup window:
-        //
-        $scope.loginDone = function( ){
-            $log.info('login is done');
-            if ( popupWindow !== null ){
-                popupWindow.close();
-                popupWindow = null;
-            }
-
-            $scope.loginDetails = {};   // we will verify this in the backend
-            $timeout(function(){$scope.play();}, 0);
-        };
-
-        var popupWindow = null;
-        $scope.play = function () {
-
-            if ( !!$scope.widget.socialLogin && !!$scope.widget.socialLogin.data  && $scope.widget.socialLogin.data.length > 0 && !$scope.loginDetails ){
-
-                var size = LoginTypesService.getIndexSize();
-
-                var left = (screen.width/2)-(size.width/2);
-                var top = (screen.height/2)-(size.height/2);
-
-                popupWindow = window.open( '/#/widgets/' + $scope.widget._id + '/login/index', 'Enter Details', 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+ size.width +', height='+ size.height +', top='+top+', left='+left);
-                return;
-            }
-
-            _resetWidgetStatus();
-            $scope.widgetStatus.state = play;
-            console.log('before check advanced');
-            var advancedParams =  _hasAdvanced() ? _getAdvanced() : null;
-            console.log('After check advanced, options=', advancedParams, '_hasAdvanced()=', _hasAdvanced() );
-
-            WidgetsService.playWidget($scope.widget, advancedParams, _isRemoteBootstrap())
+            WidgetsService.playWidget(widget, advancedParams, isRemoteBootstrap)
                 .then(function (result) {
                     console.log(['play result', result]);
-                    $scope.executionId = result.data;
-                    _pollStatus(1);
+                    var executionId = result.data;
+                    getStatus(widget, executionId);
                 }, function (err) {
                     console.log(['play error', err]);
-                    _resetWidgetStatus('We are so hot that we ran out of instances. Please try again later.');
                 });
-        };
+        }
 
-        $scope.stop = function () {
-            WidgetsService.stopWidget($scope.widget, $scope.executionId, _isRemoteBootstrap()).then(function () {
-                $scope.widgetStatus.state = stop;
-                _resetWidgetStatus();
+        function stop (widget, executionId, isRemoteBootstrap) {
+            WidgetsService.stopWidget(widget, executionId, isRemoteBootstrap).then(function () {
+                // todo return success event
             });
-        };
+        }
 
-        $scope.getFormPath = function (widget) {
-            if (widget.remoteBootstrap && widget.remoteBootstrap.cloudifyForm) {
-                return '/views/widget/forms/' + widget.remoteBootstrap.cloudifyForm + '.html';
-            }
-            return '';
-        };
+        function getOutput (widget, executionId) {
 
-
-        WidgetsService.getWidget($routeParams.widgetId).then(function (result) {
-            $scope.widget = result.data;
-        });
-
-
-        var emptyList = [];
-
-        function _getOutput (widget) {
-            $log.debug('> > > get output, widget id: ', widget ? widget._id : '', ', execution id: ', $scope.executionId);
-
-            if (!widget || !$scope.executionId) {
-                $scope.output = emptyList;
-            }
-
-            WidgetsService.getOutput(widget, $scope.executionId)
+            WidgetsService.getOutput(widget, executionId)
                 .then(function (result) {
-                    $scope.output = result.data.split('\n');
+                    return result.data.split('\n');
                 }, function (err) {
                     $log.error(err);
                 });
         }
 
-        function _isRemoteBootstrap() {
-            return $scope.widget.remoteBootstrap && $scope.widget.remoteBootstrap.active;
+
+        // post outgoing messages
+        function _postMessage(name, data) {
+            $window.parent.postMessage({name: name, data: data}, $window.location.origin);
         }
 
-
-        $log.debug('listening to messages on ', $window);
+//        $log.debug('listening to messages on ', $window);
         // listen to incoming messages
         $window.addEventListener('message', function (result) {
             $log.info('- - - message received, user posted: ', result.data);
-            switch (result.data) {
+            if (!result.data) {
+                $log.error('unable to handle posted message, no data was found');
+                return;
+            }
+            var data = result.data;
+            switch (data.name) {
                 case 'play':
-
-                    // TODO call play
-
-                    // emulate outgoing output response
-                    $timeout(function () {
-                        $window.parent.postMessage('loaded', $window.location.origin);
-                    }, 1000);
-
+                    play(data.widget, data.advancedParams, data.isRemoteBootstrap);
                     break;
-                case 'data':
+                case 'stop':
+                    stop(data.widget, data.executionId, data.isRemoteBootstrap);
+                    break;
+                case 'getOutput':
+                    getOutput(data.widget, data.executionId);
                     break;
             }
         });
